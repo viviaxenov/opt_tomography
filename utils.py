c95ddd13692c87a8b4b8c6ae22adc71e7428e546
gabesonlspool <50072563+gabesonlspool@users.noreply.github.com>
2019-05-20 21:16:28 +0300

    Reworked and simlified matrix generation

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from scipy import special
+from scipy.sparse import coo_matrix, csr
 from skimage import io, img_as_ubyte
 from skimage.color import rgb2gray
 
@@ -9,16 +10,16 @@ def generate_stochastic_matrix(
         area: np.ndarray,
         detectors_x: int,
         detectors_y: int
-    ):
+):
     """
     :param area: basis for stoch.matrix
-    :param detectors_y: the amount of detectors along fixed x axis
-    :param detectors_x: the amount of detectors along fixed y axis
-    :return: np.ndarray of size (area.shape[0] * area.shape[1], detectors_x * detectors_y)
-    Generates a row-stochastic matrix (see https://en.wikipedia.org/wiki/Stochastic_matrix)
-    based on 2d numpy array specified in area.
+    :param detectors_x: the amount of detectors along fixed x axis
+    :param detectors_y: the amount of detectors along fixed y axis
+    :return: scipy sparse coord matrix
+    of size (area.shape[0] * area.shape[1], detectors_x * detectors_y)
+    Generates a row-stochastic matrix based on 2d numpy array specified in 'area'.
     It is considered that area is covered with detectors_x * detectors_y detectors
-    placed equidistantly over the array, having a chance to detect a particle within area
+    placed equidistantly over the area, having a chance to detect a particle within area
     corresponding to 2d normal
     distribution with dispersion comparable with distance between them
 
@@ -27,25 +28,20 @@ def generate_stochastic_matrix(
     """
 
     sources = detectors_x * detectors_y
-    h_x = 1.0 / (detectors_y - 1)
-    h_y = 1.0 / (detectors_x - 1)
+    h_x = 1.0 / (detectors_x - 1)
+    h_y = 1.0 / (detectors_y - 1)
     # optional, may be changed
-    dispersion_multiplier = 5.0  # square root of dispersion of sources / distance between sources
-    sigma_x = dispersion_multiplier * h_x
-    sigma_y = dispersion_multiplier * h_y
+    dispersion_multiplier = 1  # square root of dispersion of sources / distance between sources
 
-    x = np.linspace(-1.0, 1.0, 2 * area.shape[1] + 1)
-    y = np.linspace(-1.0, 1.0, 2 * area.shape[0] + 1)
+    distr_along_x = np.zeros(2 * area.shape[1])
+    distr_along_y = np.zeros(2 * area.shape[0])
+    for i in range(-dispersion_multiplier, dispersion_multiplier):
+        distr_along_x[area.shape[1] + i] = \
+            special.erf((i + 1) / (2 ** 0.5)) - special.erf(i / (2 ** 0.5))
 
-    distr_along_x = np.array(
-        [special.erf(x[i + 1] / sigma_x / 2 ** 0.5) -
-         special.erf(x[i] / sigma_x / 2 ** 0.5)
-         for i in range(2 * area.shape[1])])
-
-    distr_along_y = np.array(
-        [special.erf(y[i + 1] / sigma_y / 2 ** 0.5) -
-         special.erf(y[i] / sigma_y / 2 ** 0.5)
-         for i in range(2 * area.shape[0])])
+    for i in range(-dispersion_multiplier, dispersion_multiplier):
+        distr_along_y[area.shape[0] + i] = \
+            special.erf((i + 1) / (2 ** 0.5)) - special.erf(i / (2 ** 0.5))
 
     stochastic_matrix = np.array([])
 
@@ -53,51 +49,67 @@ def generate_stochastic_matrix(
         for j in range(detectors_x):
 
             source_prob_matrix = np.zeros(area.shape)
-            for l in range(area.shape[1]):
-                for m in range(area.shape[0]):
-                    offset_x = int(round(area.shape[1] * (1 - h_x * i)))
-                    offset_y = int(round(area.shape[0] * (1 - h_y * j)))
-                    source_prob_matrix[m][l] = 1 / 4 *\
-                        distr_along_x[offset_x + l] *\
-                        distr_along_y[offset_y + m]
+            offset_x = int(round(area.shape[1] * h_x * j))
+            offset_y = int(round(area.shape[0] * h_y * i))
+            for l in range(-2, 2):
+                for m in range(-2, 2):
+                    if (((offset_y + m) < 0) or
+                            ((offset_y + m) > area.shape[0] - 1) or
+                            ((offset_x + l) < 0) or
+                            ((offset_x + l) > area.shape[1] - 1)
+                    ):
+                        continue
+                    source_prob_matrix[offset_y + m][offset_x + l] = 1 / 4 * \
+                        distr_along_y[area.shape[0] + m] * \
+                        distr_along_x[area.shape[1] + l]
 
-            stochastic_matrix = np.concatenate(
-                (stochastic_matrix, source_prob_matrix.flatten()),
-                axis=None
+            stochastic_matrix = np.append(
+                stochastic_matrix,
+                source_prob_matrix.flatten(),
+                axis=0
             )
 
-    return stochastic_matrix.reshape(sources, area.shape[0] * area.shape[1])
+    stochastic_matrix = stochastic_matrix.reshape((sources, area.shape[0] * area.shape[1]))
+    rows, cols = np.nonzero(stochastic_matrix)
+    data = stochastic_matrix[np.nonzero(stochastic_matrix)]
+    return coo_matrix((data, (rows, cols)))
 
 
-def create_noisy_image(filename='tiger.jpg'):
+def create_detector_image(img: np.ndarray, a: csr):
     """
-    :param filename: string
-    :return: boolean
-    Generates a image distorted with Poisson noise, based on image
-    specified in filename, and saves it into current working directory with the same
-    extension. Returns True if the image creation was successful, False otherwise
+    :param img: np.array representation of image
+    :param a: sparse detector likelihood matrix
+    :return: np.array image, which will be obtained by detector with the given
+    likelihood matrix, if the sources radiate as img
     """
 
-    img = None
+    print(a.toarray().shape)
+    detector_img = a @ img.flatten()
+    return detector_img
+
+
+def create_noisy_image(filename, a: csr):
+    """
+    :param filename: path to image
+    :param a: scipy.csr stochastic matrix
+    :return: (image shape, original image, noisy image) tuple
+    Returns a vector-like representation of image distorted with Poisson noise, based on file
+    specified in 'filename', and  stochastic matrix, specified in 'a'
+    """
+
     filename = os.path.join(os.getcwd(), filename)
     try:
         img = rgb2gray(img_as_ubyte(io.imread(filename))) * 255
     except FileNotFoundError as err:
         print(err.args)
-        return False
+        return None
 
     # It is unknown how to convert random samples into scale of white color intensity
     # optional, may be changed
     _lambda = 100.0
 
-    noise = np.random.poisson(_lambda, img.shape)
+    detector_img = create_detector_image(img, a)
+    noise = a @ np.random.poisson(_lambda, img.shape[0] * img.shape[1])
 
-    img = np.array([i if i <= 255 else 255 for i in np.nditer(img + noise)]).reshape(
-        img.shape[0],
-        img.shape[1],
-    )
+    return img.shape, img.flatten(), detector_img + noise
 
-    result_file = filename.split(".")[0] + "_noisy." + filename.split(".")[1]
-    io.imsave(result_file, img.astype(np.uint8))
-
-    return True
