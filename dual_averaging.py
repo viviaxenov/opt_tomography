Fixed dual averaging method, added explicit formula for step, but now only works for R++

dual_averaging.py
@@ -2,53 +2,40 @@ import numpy as np
 import scipy as sp
 import scipy.optimize, scipy.stats
 
-class DualAveragingMethod:
-    def __init__(self, x_0: np.ndarray, f, sg, eta: np.float64, bounds: sp.optimize.Bounds, mirror_map='Rn'):
+class DualAveragingRPP:
+    def __init__(self, x_0: np.ndarray, f, sg, eta: np.float64):
         """"
-        Class for optimizing function using dual averaging method
+        Class for optimizing function using dual averaging method on R++ with mirror map \sum{x_i \log{x_i}}
         Args:
             x_0 - initial guess
             f - function to be optimized
             sg - function x: np.ndarray -> sg(x): np.ndarray returns a member of subgradient in point x
             eta - gradient descent parameter
-            bounds - instance of scp.optimize.Bounds - defines the convex set X in which the function should be optimized
-            mirror_map - string 'Rn' or 'R++' - defines mirror map function (see Bubeck, p.298 for definition)
         """
         self.f = f
         self.sg = sg
         self.eta = eta
+
+        if(np.any(x_0 <= 0)):
+            raise ValueError("Some of x_0 components are negative")
+
         self.g = sg(x_0)
         self.x = x_0
         self.sum = x_0
         self.x_avg = x_0
         self.x_min = x_0
-        self.bounds = bounds
         self.iterations = 0
-        self.F = []
-        self.jac = []
+        self.F = lambda x: x.T@np.log(x)
 
-        if mirror_map == 'Rn':
-            self.F = lambda x: 0.5*np.dot(x, x)
-            self.jac = lambda x: self.eta*self.g + x
-        elif mirror_map == 'R++':
-            self.F = lambda x: np.log(x).T@x
-            self.jac = lambda x: self.eta*self.g + np.log(x) + 1.
-        else:
-            raise ValueError('Unknown mirror map type. Can only be \'Rn\' or \'R++\'')
-
-        self.step_function = lambda x: self.eta*np.dot(self.g, x) + self.F(x)
 
     def iteration(self):
-        res: scipy.optimize.OptimizeResult = scipy.optimize.minimize(self.step_function, self.x, jac=self.jac,
-                                                                     bounds=self.bounds, method='L-BFGS-B')
+        x_new = np.exp(-self.eta*self.g - np.log(self.x) - 1.)
         self.iterations += 1
-        if not res.success:
-            raise RuntimeError(f'Failed to make iteration {self.iterations}. Cause of termination: {res.message}')
 
-        if self.f(res.x) < self.f(self.x_min):
-            self.x_min = res.x
-        self.x = res.x
-        self.sum += res.x
+        if self.f(x_new) < self.f(self.x_min):
+            self.x_min = x_new
+        self.x = x_new
+        self.sum += x_new
         self.x_avg = self.sum/self.iterations
         self.g += self.sg(self.x)
 